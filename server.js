d1991905f912e5da0d78ac4a9212ded45fa3c44b
tejashreeSalvi <salvitejashree1999@gmail.com>
2020-12-24 01:01:00 +0530

    Portfolio Website

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/personal-portfoli-angular'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/dist/personal-portfoli-angular/index.html'));});
+app.listen(process.env.PORT || 8080);
